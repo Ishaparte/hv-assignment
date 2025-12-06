12a269cfb683c0c52a0dbeb84127c8761a8560bf
Ishaparte <ishaparte4@gmail.com>
2025-12-06 12:38:25 +0530

    final commit

--- src/lib/client.js
+++ src/lib/client.js
@@ -1,8 +1,9 @@
-import { createClient } from "@sanity/client";
+import { createClient } from "next-sanity";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: "production",
-  apiVersion: "2023-05-01",
-  useCdn: false
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  useCdn: false,
+  token: process.env.SANITY_API_READ_TOKEN, // required for Vercel
 });
